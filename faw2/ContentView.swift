789154a70591f6cd9460579134bcd1ce4b1d0753
fawwaz2828 <fawwazwibowo96@gmail.com>
2026-04-02 08:53:22 +0800

    Initial Commit

---
+++ faw2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  faw2
+//
+//  Created by 33 on 2026/4/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ faw2/faw2App.swift
@@ -0,0 +1,17 @@
+//
+//  faw2App.swift
+//  faw2
+//
+//  Created by 33 on 2026/4/2.
+//
+
+import SwiftUI
+
+@main
+struct faw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
